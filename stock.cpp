087bbf8d59a7aadfea0598c3448735439d202b0a
dangjustintime <dangjustintime@gmail.com>
2018-02-22 01:11:02 -0500

    added stock.cpp

---
+++ stock.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+
+struct Stock {
+	std::string name;
+	double price;
+	double weeklyRate;
+	double monthlyRate;
+	double yearlyRate;
+	double wholeRate;
+	double dividend;
+};
+
+int main() {
+
+	return 0;
+}
