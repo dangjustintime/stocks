2004235ff099d1524d1cf815e456111194b46e29
dangjustintime <dangjustintime@gmail.com>
2018-02-25 19:49:41 -0500

    added default constructor for portfolio

--- portfolio.h
+++ portfolio.h
@@ -2,20 +2,47 @@
 #define PORTFOLIO_H
 
 #include "stock.h"
+#include <vector>
+
 class Portfolio {
 	public:
 		//operators
-
+		
 		//constructors
+		Portfolio() : numStocks{0}, stockValue{0.0}, cashValue{0.0}, numYears{0} { stockVector(); };
+		Portfolio(double numStocks = 0, double stockValue = 0.0, double cashValue = 0.0, int numYears 0) :
+			numStocks{numStocks}, stockValue{stockValue}, cashValue{cashValue},
+			numYears{numYears} { stockVector(); };
+		Portfolio(Portfolio& portfolio) :
+			numStocks{portfolio.getNumStocks()}, stockValue{portfolio.getStockValue()}, cashValue{portfolio.getCashValue()},
+				for(int i = 0; <= portfolio.stockVector.size(); i++) {
+					/*
+					add portfolio stocks to vector stocks
+					*/	
+				}
+			};
 
 		//getters and setters	
-	private:
+		int getNumStocks();
+		double getValue();
+		double getStockValue();
+		double getCashValue();
+		void setCashValue(double);
 		
+	private:
+		Vector <Stock> stockVector;
+		int numStocks;
+		double value = stockValue + cashValue;
+		double stockValue;
+		double cashValue;
+		int numYears;
 }
 
 //defintions
 //operators
+
 //constructors
+
 //getters and setters
 
 #endif
