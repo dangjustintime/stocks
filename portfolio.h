3d2b04aecebc8162d35e41e937837edd3d11b1d7
dangjustintime <dangjustintime@gmail.com>
2018-03-12 15:24:38 -0400

    added add/remove stock to portfolio.h

--- portfolio.h
+++ portfolio.h
@@ -30,8 +30,14 @@ class Portfolio {
 		double getCashValue();
 		void setCashValue(double);
 
-                    void addStock(Stock);
-                    void removeStock(std::string);
+                    //add/remove stocks
+                    void addStock(const Stock&);
+                    void addStock(Stock&&);
+                    void removeStock(const std::string&);
+
+                    //buy/sell stocks
+                    void buyStock(const Stock&);
+                    void sellStock(const std::string&);
 	private:
                     std::map<std::string, Stock> stockMap;
 		int numStocks;
@@ -103,5 +109,34 @@ double Portfolio::getTotalValue() { return stockValue + cashValue; };
 double Portfolio::getStockValue() { return stockValue; };
 double Portfolio::getCashValue() { return cashValue; };
 void Portfolio::setCashValue(double cashValue) { this->cashValue = cashValue; }
+
+//add stock (copy version)
+void Portfolio::addStock(const Stock& stock) {
+          stockMap.insert(std::pair<std::string, Stock>(stock.getName(), stock));
+          numStocks++;
+          stockValue+=stock.getPrice();
+}
+//add stock (move version)
+void Portfolio::addStock(Stock&& stock) {
+          stockMap.insert(std::pair<std::string, Stock>(stock.getName(), std::move(stock)));
+          numStocks++;
+          stockValue+=stock.getPrice();
+	stock.setName("----");
+	stock.setPrice(0.0);
+	stock.setDailyRate(0.0);
+	stock.setWeeklyRate(0.0);
+	stock.setMonthlyRate(0.0);
+	stock.setThreeMonthRate(0.0);
+	stock.setYearlyRate(0.0);
+	stock.setGeneralRate(0.0);
+	stock.setDividend(0.0);
+          
+}
+//remove stock
+void Portfolio::removeStock(const std::string& key) {
+        numStocks--;
+        stockValue-= stockMap[key].getPrice();
+        stockMap.erase(key);
+}
 
 #endif
